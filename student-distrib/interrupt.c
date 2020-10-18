83ca0d5ff1deb86a72d396508f26430b16f015e4
gnodipac886 <ericdong886@yahoo.com>
2020-10-18 00:17:20 -0500

    hard coded all exceptions

--- student-distrib/interrupt.c
+++ student-distrib/interrupt.c
@@ -1,11 +1,12 @@
 #include "interrupt.h"
 #include "x86_desc.h"
+#include "lib.h"
 
 void __init_idt__(){
 	int i;
 
 	for(i = 0; i < 20; i++){
-		if(i != 15){ 							// we want to skip the intel reserved ones
+		if(i != 9 || i != 15){ 							// we want to skip the intel reserved ones
 			idt[i].seg_selector = KERNEL_CS;
 			idt[i].reserved4 = 0;
 			idt[i].reserved3 = i == 2 ? 0 : 1; 	// #2 is an interrupt
@@ -18,29 +19,27 @@ void __init_idt__(){
 		}
 	}
 
-	SET_IDT_ENTRY(idt[0x00], divide_error); 			// divide by zero error
-	SET_IDT_ENTRY(idt[0x01], divide_error); 			// reserved
-	SET_IDT_ENTRY(idt[0x02], divide_error); 			// divide by zero error
-	SET_IDT_ENTRY(idt[0x03], divide_error); 			// divide by zero error
-	SET_IDT_ENTRY(idt[0x04], divide_error); 			// divide by zero error
-	SET_IDT_ENTRY(idt[0x05], divide_error); 			// divide by zero error
-	SET_IDT_ENTRY(idt[0x06], divide_error); 			// divide by zero error
-	SET_IDT_ENTRY(idt[0x07], divide_error); 			// divide by zero error
-	SET_IDT_ENTRY(idt[0x08], divide_error); 			// divide by zero error
-	SET_IDT_ENTRY(idt[0x09], divide_error); 			// divide by zero error
-	SET_IDT_ENTRY(idt[0x0A], divide_error); 			// divide by zero error
-	SET_IDT_ENTRY(idt[0x0B], divide_error); 			// divide by zero error
-	SET_IDT_ENTRY(idt[0x0C], divide_error); 			// divide by zero error
-	SET_IDT_ENTRY(idt[0x0D], divide_error); 			// divide by zero error
-	SET_IDT_ENTRY(idt[0x0E], divide_error); 			// divide by zero error
-	SET_IDT_ENTRY(idt[0x0F], divide_error); 			// divide by zero error
-	SET_IDT_ENTRY(idt[0x10], divide_error); 			// divide by zero error
-	SET_IDT_ENTRY(idt[0x11], divide_error); 			// divide by zero error
-	SET_IDT_ENTRY(idt[0x12], divide_error); 			// divide by zero error
-	SET_IDT_ENTRY(idt[0x13], divide_error); 			// divide by zero error
+	SET_IDT_ENTRY(idt[DE], divide_error); 			// divide by zero error
+	SET_IDT_ENTRY(idt[DB], divide_error); 			// reserved
+	SET_IDT_ENTRY(idt[BP], divide_error); 			// divide by zero error
+	SET_IDT_ENTRY(idt[OF], divide_error); 			// divide by zero error
+	SET_IDT_ENTRY(idt[BR], divide_error); 			// divide by zero error
+	SET_IDT_ENTRY(idt[UD], divide_error); 			// divide by zero error
+	SET_IDT_ENTRY(idt[NM], divide_error); 			// divide by zero error
+	SET_IDT_ENTRY(idt[DF], divide_error); 			// divide by zero error
+	SET_IDT_ENTRY(idt[TS], divide_error); 			// divide by zero error
+	SET_IDT_ENTRY(idt[NP], divide_error); 			// divide by zero error
+	SET_IDT_ENTRY(idt[SS], divide_error); 			// divide by zero error
+	SET_IDT_ENTRY(idt[GP], divide_error); 			// divide by zero error
+	SET_IDT_ENTRY(idt[PF], divide_error); 			// divide by zero error
+	SET_IDT_ENTRY(idt[MF], divide_error); 			// divide by zero error
+	SET_IDT_ENTRY(idt[AC], divide_error); 			// divide by zero error
+	SET_IDT_ENTRY(idt[MC], divide_error); 			// divide by zero error
+	SET_IDT_ENTRY(idt[XF], divide_error); 			// divide by zero error
 }
 
 void divide_error(){
+	cli();
 	printf("Exception: divide error");
 	exception();
 }
