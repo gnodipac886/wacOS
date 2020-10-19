6977c98390e348ebdcf3fcb1d05a6e02e7ad7294
Shang <slin2515@gmail.com>
2020-10-18 20:24:12 -0500

    keyboard implemented

--- student-distrib/keyboard.h
+++ student-distrib/keyboard.h
@@ -1,9 +1,14 @@
 #ifndef _KEYBOARD_H
 #define _KEYBOARD_H
 
-#define KB_IRQ 	0x01
+#define KB_PORT 0x60 // IO port for keyboard
+#define KB_IRQ 	0x01 // keyboard interrupt
 
+#define BUFFER_RS 0xFF // keyboard buffer overrun
+
+// initialization of the keyboard for interrupt
 extern void __keyboard_init__();
+// takes in the input and display to screen
 extern void handle_keyboard_interrupt();
 
 #endif /* _KEYBOARD_H */
