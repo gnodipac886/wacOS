42f6677035dc10405d9f06e3ad363000d659d7bd
Phoebe <phoebec2@illinois.edu>
2020-10-19 13:43:28 -0500

    rtc additional stuff optional

--- student-distrib/rtc.c
+++ student-distrib/rtc.c
@@ -4,14 +4,20 @@
 
 /* Initialize RTC */
 void rtc_init(){
-
+    cli();
     /*set IRQ8 */
-    outb(RTC_STATUS_REG+0xB, RTC_IO_PORT);  // Select RTC status register B (offset = 0xB)
+    outb(RTC_STATUS_REG+0x0B, RTC_IO_PORT);  // Select RTC status register B (offset = 0xB)
     uint8_t reg_value = inb(CMOS_IO_PORT);  // Read register B value
-    outb(RTC_STATUS_REG+0xB, RTC_IO_PORT);  // Select RTC status register B 
+    outb(RTC_STATUS_REG+0x0B, RTC_IO_PORT);  // Select RTC status register B 
     outb(reg_value | 0x40, CMOS_IO_PORT);   // Turn on Register B bit 6
 
     enable_irq(RTC_IRQ);                    // Enable interrupt for RTC on PIC
+
+    /*Just in case: clear register C to allow interrupt.*/
+    outb(0x0C, RTC_IO_PORT);                // Select RTC status register C
+    inb(CMOS_IO_PORT);                      // Dump the content
+
+    sti();
 }
 
 /* handle_rtc_interrupt */
