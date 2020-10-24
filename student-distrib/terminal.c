1e0f3fa7b84a9407e4284ab9c484380425ec22a2
Shang <slin2515@gmail.com>
2020-10-24 06:15:56 -0500

    implemented terminal read/write -still needs testing

---
+++ student-distrib/terminal.c
@@ -0,0 +1,77 @@
+#include "keyboard.h"
+#include "lib.h"
+
+int terminal_open(){
+    //  initializes terminal stuff (or nothing)
+    return 0;
+}
+
+int terminal_close(){
+    // clears any terminal specific variables or do nothing
+    return 0;
+}
+
+/* terminal_read
+ *      Description: Reads in input from the keyboard and store it in buf
+ * 		Inputs: fd - file descriptor, should be 0 for reading
+ *              buf - buffer that stores the keyboard inputs
+ *              nbytes - number of bytes to read
+ * 		Return Value: 0 - error, invalid pointer, or empty
+ *                    number of bytes of characters
+ * 		Function: Reads keyboard buffer and store it in buf. Checks for \n
+ *                that signals the end of the keyboard buffer and writes \0 to
+ *                the end of the buf.
+ *		Side Effects: none
+ */
+int32_t terminal_read(int32_t fd, const void* buf, int32_t nbytes){
+    // check for valid buf
+    if(buf == NULL){
+        return 0;
+    }
+    int32_t counter = 0;            // counter for iterating keyboard buffer
+    char* kb_buf = get_kb_buf();    // pointer to the keyboard buffer
+    // reads each char in keyboard buffer and checks for \n or reached nbytes
+    while(counter != nbytes){
+        if(counter == (nbytes - 1)){
+            buf[nbytes - 1] = '\0'; // end of the buffer
+        } else if(kb_buf[counter] == '\n'){
+            buf[counter] = '\0';    // signals the end of the buffer
+            return counter + 1;     // total bytes read
+        } else{
+            buf[counter] = kb_buf[counter];
+        }
+        counter++;
+    }
+
+    return nbytes;
+}
+
+/* terminal_write
+ *      Description: Writes data in buf to the screen
+ * 		Inputs: fd - file descriptor, should be 0 for reading
+ *              buf - buffer that stores the data
+ *              nbytes - number of bytes to write
+ * 		Return Value: -1 - error, invalid pointer, or empty
+ *                    number of bytes of characters written
+ * 		Function: Writes nbytes of char to the screen from buf.
+ *		Side Effects: none
+ */
+int32_t terminal_write(int32_t fd, const void* buf, int32_t nbytes){
+    // check for valid buf
+    if(buf == NULL){
+        return -1;
+    }
+    int32_t i;                      // counter
+    // loop through buf until all nbytes are written to the screen
+    for(i = 0; i < nbytes; i++){
+        if(i == 80){
+            printf("\n");           // newline reached max of the screen width
+        }
+
+        if(buf[i] != '\0'){
+            printf("%c", buf[i]);   // prints each char in buf, ignores null bytess
+        }
+
+    }
+    return nbytes;
+}
---
+++ student-distrib/terminal.h
@@ -0,0 +1,17 @@
+#ifndef _TERMINAL_H
+#define _TERMINAL_H
+
+#define BUF_SZ      128 //max size of buffer = 128 bytes
+#define NUM_COLS    80 // video cols
+#define NUM_ROWS    25 // video rows
+
+/* initializes terminal */
+extern int terminal_open();
+/* clears the terminal */
+extern int terminal_close();
+/* reads from keyboard or input */
+extern int32_t terminal_read(int32_t fd, const void* buf, int32_t nbytes);
+/* writes to the screen */
+extern int32_t terminal_write(int32_t fd, const void* buf, int32_t nbytes);
+
+#endif /*_TERMINAL_H */
