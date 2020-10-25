ae5959998f29e15ec71c35fb3f4e0f60f14243e3
gnodipac886 <ericdong886@yahoo.com>
2020-10-25 03:09:35 -0500

    fixed directory read

--- student-distrib/filesystem.c
+++ student-distrib/filesystem.c
@@ -2,30 +2,205 @@
 #include "lib.h"
 
 static void * filesystem;
-file_descriptor_t fd_arr[8];
+boot_block_t* boot_block;
+inode_t* inodes;
+data_block_t* data_ptr;
+
+file_descriptor_t fd_arr[MAX_FILES_OPEN];
+
+uint32_t rtc_file_ops[4] = {(uint32_t)_open, (uint32_t)rtc_read, (uint32_t)_write, (uint32_t)_close};
+uint32_t dir_file_ops[4] = {(uint32_t)_open, (uint32_t)dir_read, (uint32_t)_write, (uint32_t)_close};
+uint32_t file_file_ops[4] = {(uint32_t)_open, (uint32_t)file_read, (uint32_t)_write, (uint32_t)_close};
+
+// helper functions
+// char* _get_file_name(char* fname);
 
 void __init_filesystem__(void * filesystem_ptr){
-	filesystem = filesystem_ptr;
+	int i;
+	filesystem 	= filesystem_ptr;
+	boot_block 	= (boot_block_t*)filesystem;
+	inodes 		= (inode_t*)filesystem;
+	data_ptr 	= (data_block_t*)filesystem;
+
+	// set up the file descriptor array
+	for(i = 0; i < MAX_FILES_OPEN; i++){
+		fd_arr[i].jmp_table = NULL;
+		fd_arr[i].inode = -1;
+		fd_arr[i].file_position = -1;
+		fd_arr[i].flags = FILE_NOT_USE;
+	}
 }
 
-//!!!!!!!!!!!!!!!!!!!! make sure fname is \0 terminated               !!!!!!!!!!!!!!!!!!!!!!!!!!!
+int32_t _open(const uint8_t* fname){
+	int i;
+	int fd = FIRST_FILE_IDX;
+	dentry_t dentry;
+
+	// sanity check for fname
+	for(i = 0; i < MAX_NAME_LEN + 1; i++){
+		// found null, continue
+		if(fname[i] == '\0'){
+			break;
+		}
+
+		// didn't find it, return fail
+		if(i == MAX_NAME_LEN && fname[i] != '\0'){
+			return -1;
+		}
+	}
+
+	// check if the file is found at all
+	if(read_dentry_by_name(fname, &dentry) == -1){
+		return -1;
+	}
+
+	// loop through the array to see which 
+	while(fd_arr[fd].flags){
+		fd++;
+
+		// if the whole file array is full, return fail
+		if(fd >= 8){
+			return -1;
+		}
+	}
+
+	// set the jump table for the file operations
+	switch(dentry.type){
+		case RTC_TYPE:
+			fd_arr[fd].jmp_table = rtc_file_ops;
+			break;
+
+		case DIR_TYPE:
+			fd_arr[fd].jmp_table = dir_file_ops;
+			break;
+
+		case FILE_TYPE:
+			fd_arr[fd].jmp_table = file_file_ops;
+			break;
+
+		default:
+			return -1;
+	}
+
+	// set up the rest of the file descriptor
+	fd_arr[fd].inode = dentry.inode;
+
+	if(dentry.type == RTC_TYPE || dentry.type == DIR_TYPE){
+		fd_arr[fd].inode = 0;
+	}
+
+	fd_arr[fd].file_position = 0;
+	fd_arr[fd].flags = FILE_IN_USE;
+
+	return fd;
+}
+
+int32_t _close(int32_t fd){
+	// see if the file descriptor index is valid
+	if(fd >= 8 || fd < 2){
+		return -1;
+	}
+
+	// check if the file is used at all
+	if(fd_arr[fd].flags == FILE_NOT_USE){
+		return -1;
+	}
+
+	// reset the file
+	fd_arr[fd].jmp_table = NULL;
+	fd_arr[fd].inode = -1;
+	fd_arr[fd].file_position = -1;
+	fd_arr[fd].flags = FILE_NOT_USE;
+
+	return 0;
+}
+
+// needs to complete
+int32_t _write(int32_t fd, void* buf, int32_t nbytes){
+	if(fd >= 8 || fd < 2 || fd_arr[fd].flags == FILE_NOT_USE){
+		return -1;
+	}
+
+	if(fd_arr[fd].jmp_table == rtc_file_ops){
+		// call rtc write here
+
+		return 0;
+	}
+
+	return -1;
+}
+
+// needs to complete
+int32_t rtc_read(int32_t fd, void* buf, int32_t nbytes){
+	// add some black magic here to wait for an interrupt to happen
+	return 0;
+}
+
+int32_t dir_read(int32_t fd, void* buf, int32_t nbytes){
+	char dir_name[MAX_NAME_LEN + 1];
+
+	// sanity checks
+	if(fd >= 8 || fd < 2 || fd_arr[fd].flags == FILE_NOT_USE || fd_arr[fd].jmp_table != dir_file_ops){
+		return -1;
+	}
+
+	// see if we have already read all the file directories
+	if(fd_arr[fd].file_position == boot_block->num_entries){
+		return 0;
+	}
+
+	// copy it over to check for '\0' at the end
+	strncpy(dir_name, (boot_block->files)[fd_arr[fd].file_position].name, MAX_NAME_LEN);
+
+	// if there isn't a '\0' character at the end, add one
+	if(dir_name[MAX_NAME_LEN - 1] != '\0'){
+		dir_name[MAX_NAME_LEN] = '\0';
+	}
+
+	// bring the current directory name over
+	strcpy((char*)buf, dir_name);
+
+	// update the file position
+	fd_arr[fd].file_position++;
+
+	return nbytes;
+}
+
+int32_t file_read(int32_t fd, void* buf, int32_t nbytes){
+	int offset = 0;
+
+	if(fd >= 8 || fd < 2 || fd_arr[fd].flags == FILE_NOT_USE || fd_arr[fd].jmp_table != file_file_ops){
+		return -1;
+	}
+
+	return read_data(fd_arr[fd].inode, offset, (uint8_t*)buf, nbytes);
+}
+
 int32_t read_dentry_by_name(const uint8_t* fname, dentry_t* dentry){
 	// initialize counter
 	int i;
-	boot_block_t* boot_block = (boot_block_t*)filesystem;
-	char* curr_file_name;
+	char curr_file_name[33];
 	char* file_name = (char*)fname;
-	dentry curr_dentry;
+	dentry_t curr_dentry;
 
 	// loop through all the entries to see which one is the right file
 	for(i = 0; i < boot_block->num_entries; i++){
 		curr_dentry = (boot_block->files)[i];
-		curr_file_name = _get_file_name(curr_dentry.name);
+		// curr_file_name = _get_file_name(curr_dentry.name);
+		
+		// check if the name has the right ending
+		if((curr_dentry.name)[MAX_NAME_LEN - 1] != '\0'){ 										// -1 for 0 indexing
+			strncpy((int8_t*)curr_file_name, (int8_t*)(curr_dentry.name), MAX_NAME_LEN); 		// copy over the original string
+			curr_file_name[MAX_NAME_LEN] = '\0';
+		}
+		else{
+			strcpy((int8_t*)curr_file_name, (int8_t*)(curr_dentry.name)); 						// copy over the original string
+		}
 
 		// check if the strings are equal
 		if(!(strncmp((int8_t*)curr_file_name, (int8_t*)file_name, strlen((int8_t*)file_name)))){
 			// copy over all the pieces in the directory entry
-			strncmp((int8_t*)(dentry->name), (int8_t*)curr_dentry.name, MAX_NAME_LEN);
+			strcpy((int8_t*)(dentry->name), (int8_t*)curr_dentry.name);
 			dentry->type = curr_dentry.type;
 			dentry->inode = curr_dentry.inode;
 			return 0;
@@ -36,9 +211,8 @@ int32_t read_dentry_by_name(const uint8_t* fname, dentry_t* dentry){
 	return -1;
 }
 
-int32_t read_dentry_by_index(uint32_t index, dentry* dentry){
-	dentry curr_dentry;
-	boot_block_t* boot_block = (boot_block_t*)filesystem;
+int32_t read_dentry_by_index(uint32_t index, dentry_t* dentry){
+	dentry_t curr_dentry;
 
 	// if the index is larger, then its a fail
 	if(index >= boot_block->num_entries){
@@ -52,14 +226,13 @@ int32_t read_dentry_by_index(uint32_t index, dentry* dentry){
 	strncmp((int8_t*)(dentry->name), (int8_t*)curr_dentry.name, MAX_NAME_LEN);
 	dentry->type = curr_dentry.type;
 	dentry->inode = curr_dentry.inode;
+
+	return 0;
 }
 
 // actually does the data retrieval
 // remember when using this, make sure it's the right file type, not dir or RTC
 int32_t read_data(uint32_t inode, uint32_t offset, uint8_t* buf, uint32_t length){
-	boot_block_t* boot_block = (boot_block_t*)filesystem;
-	inode_t* inodes = (inode_t*)filesystem;
-	data_block_t* data_ptr = (data_block_t*)filesystem;
 	data_block_t* curr_data_block;
 	inode_t curr_inode;
 
@@ -76,7 +249,7 @@ int32_t read_data(uint32_t inode, uint32_t offset, uint8_t* buf, uint32_t length
 	}
 
 	// otherwise assign the inode
-	curr_inode = inodes[inode];
+	curr_inode = inodes[inode + 1];
 
 	// more sanity checks, see if offset and length are valid
 	if(offset >= curr_inode.length || length >= curr_inode.length){
@@ -96,9 +269,9 @@ int32_t read_data(uint32_t inode, uint32_t offset, uint8_t* buf, uint32_t length
 			num_mem2_cpy = num_bytes_left;
 		}
 		else{
-			num_mem2_cpy = BLOCK_SIZE - ((offset + num_copied) % BLOCK_SIZE)
+			num_mem2_cpy = BLOCK_SIZE - ((offset + num_copied) % BLOCK_SIZE);
 		}
-		memcpy((void*)(buf + num_copied), (void*)data_loc);
+		memcpy((void*)(buf + num_copied), (void*)data_loc, num_mem2_cpy);
 
 		// update trackers
 		num_copied += num_mem2_cpy; 			// realized could just use one tracker instead... too late
@@ -112,19 +285,15 @@ int32_t read_data(uint32_t inode, uint32_t offset, uint8_t* buf, uint32_t length
 	return offset + num_copied == curr_inode.length ? 0 : num_copied;
 }
 
+// char* _get_file_name(char* fname){
+// 	char name[33];
 
+// 	// check if the name is 32 characters long by checking the last character
+// 	if(fname[MAX_NAME_LEN - 1] != '\0'){ 							// -1 for 0 indexing
+// 		strncpy((int8_t*)name, (int8_t*)fname, MAX_NAME_LEN); 		// copy over the original string
+// 		name[32] = '\0';
+// 		return name;
+// 	}
 
-char* _get_file_name(char* fname){
-	// counter for for loop
-	int i;
-	char* name[33];
-
-	// check if the name is 32 characters long by checking the last character
-	if(fname[MAX_NAME_LEN - 1] != '\0'){ 							// -1 for 0 indexing
-		strncpy((int8_t*)name, (int8_t*)fname, MAX_NAME_LEN); 		// copy over the original string
-		name[32] = '\0';
-		return name;
-	}
-
-	return fname;
-}
+// 	return fname;
+// }
--- student-distrib/filesystem.h
+++ student-distrib/filesystem.h
@@ -10,6 +10,16 @@
 #define 	MAX_DATA_BLOCK 	(BLOCK_SIZE - SLOT_SIZE) / SLOT_SIZE 	// 1023
 #define 	DIR_ENTRY_SIZE	64
 
+#define 	MAX_FILES_OPEN 	8
+#define 	FIRST_FILE_IDX 	2
+
+#define 	RTC_TYPE		0
+#define 	DIR_TYPE 		1
+#define 	FILE_TYPE 		2
+
+#define 	FILE_IN_USE 	1
+#define 	FILE_NOT_USE 	0
+
 typedef struct dentry{
 	char 		name[MAX_NAME_LEN];
 	uint32_t 	type;
@@ -27,21 +37,29 @@ typedef struct boot_block{
 	uint32_t 	num_inode;
 	uint32_t 	num_data_block;
 	uint8_t 	reserved[52];
-	dentry 		files[MAX_DIR_ENTRY]
+	dentry_t 	files[MAX_DIR_ENTRY];
 } boot_block_t;
 
 typedef struct file_descriptor{
-	uint32_t 	jmp_table;
-	uint32_t 	fd_inode;
+	uint32_t* 	jmp_table;
+	uint32_t 	inode;
 	uint32_t 	file_position;
-	uint32_t 	flags;
+	uint32_t 	flags;					// 1 for in use, 0 for vacant
 } file_descriptor_t;
 
 typedef struct data_block{
 	uint8_t 	data[BLOCK_SIZE];
 } data_block_t;
 
-// open, close, read, write
-// dir_open, file_open
+void __init_filesystem__(void * filesystem_ptr);
+int32_t _open(const uint8_t* fname);
+int32_t _close(int32_t fd);
+int32_t _write(int32_t fd, void* buf, int32_t nbytes);
+int32_t rtc_read(int32_t fd, void* buf, int32_t nbytes);
+int32_t dir_read(int32_t fd, void* buf, int32_t nbytes);
+int32_t file_read(int32_t fd, void* buf, int32_t nbytes);
+int32_t read_dentry_by_name(const uint8_t* fname, dentry_t* dentry);
+int32_t read_dentry_by_index(uint32_t index, dentry_t* dentry);
+int32_t read_data(uint32_t inode, uint32_t offset, uint8_t* buf, uint32_t length);
 
 #endif /* _FILESYSTEM_H */
